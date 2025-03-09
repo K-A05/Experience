aaa5bc96736607d382ad654abcb02cdbfc3ec6cd
K-A05 <karan.anand511@gmail.com>
2025-03-09 03:10:22 +0000

    Updated

---
+++ Game project final/Variables.js
@@ -0,0 +1,25 @@
+var gameChar;
+var floorPos_y;
+var game_socre;
+
+var isLeft;
+var isRight;
+var isPlummeting;
+var isFalling;
+
+// environment variables
+var collectable;
+var collectables;
+var tree; 
+var clouds; 
+var mountains; 
+
+var flagpole;
+var lives;
+
+var cameraPosX;
+
+var myFont;
+
+var plarforms;
+var onPlatform;
